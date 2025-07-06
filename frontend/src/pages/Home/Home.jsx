38d25f81cbb3801e186fcd8eb4113b6134b91049
Pranay Gedam <pranayg176@gmail.com>
2025-07-06 19:54:03 +0530

    Added some components

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 import './Home.css'
 
 const Home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
