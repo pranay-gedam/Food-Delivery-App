3962c480f1b524cd387cf386736a036efd878482
Pranay Gedam <pranayg176@gmail.com>
2025-07-07 16:01:41 +0530

    Done with sign up page

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/Fooditem'
 import './FoodDisplay.css'
 
 const FoodDisplay = ({category}) => {
@@ -10,6 +11,13 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
+        <div className="food-display-list">
+            {food_list.map((item,index)=>{
+              if(category==="All" || category===item.category){
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+              }
+            })}
+        </div>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import AppDownload from '../../components/AppDownload/AppDownload'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import Header from '../../components/Header/Header'
@@ -14,6 +15,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload />
     </div>
   )
 }
