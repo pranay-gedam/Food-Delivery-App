febf206ac560e2149060cab99f960aec3329f4ba
Pranay Gedam <pranayg176@gmail.com>
2025-07-16 16:41:43 +0530

    Frontend code completed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,14 +4,19 @@ import header_img from "../../assets/header_img.png";
 
 const Header = () => {
   return (
-    <div className='header' style={{ background: `url(${header_img}) no-repeat`, backgroundSize: "contain" }}>
+    <div className="header"
+      style={{
+        background: `url(${header_img}) no-repeat`,
+        backgroundSize: "contain",
+      }}
+    >
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
         <button>View Menu</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header
